e6a207cc939c8bf25dc98625ba9ba82b0ce1380d
양아름 <yangareum0130@gmail.com>
2025-04-30 23:45:03 +0900

    fix: img경로 변경 빌드 테스트

--- src/_components/MemberList.jsx
+++ src/_components/MemberList.jsx
@@ -10,7 +10,7 @@ export default function MemberList() {
             <li>
               <button className="flex gap-4 items-center">
                 <img
-                  src="/src/assets/vite.svg"
+                  src="../assets/vite.svg"
                   alt="user profile img"
                   className="block w-[4rem] h-[4rem] rounded-full"
                 />
@@ -22,7 +22,7 @@ export default function MemberList() {
             <li>
               <button className="flex gap-4 items-center">
                 <img
-                  src="/src/assets/vite.svg"
+                  src="../assets/vite.svg"
                   alt="user profile img"
                   className="block w-[4rem] h-[4rem] rounded-full"
                 />
@@ -34,7 +34,7 @@ export default function MemberList() {
             <li>
               <button className="flex gap-4 items-center">
                 <img
-                  src="/src/assets/vite.svg"
+                  src="../assets/vite.svg"
                   alt="user profile img"
                   className="block w-[4rem] h-[4rem] rounded-full"
                 />
@@ -53,8 +53,8 @@ export default function MemberList() {
             <li>
               <button className="flex gap-4 items-center">
                 <img
-                  src={import.meta.env.VITE_BASE_URL + "/vite.svg"}
-                  // src="/src/assets/vite.svg"
+                  // src={import.meta.env.VITE_BASE_URL + "/vite.svg"}
+                  src="../assets/vite.svg"
                   alt="user profile img"
                   className="block w-[4rem] h-[4rem] rounded-full"
                 />

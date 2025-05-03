2751332d65af53492523d9388753204be9935865
양아름 <yangareum0130@gmail.com>
2025-05-04 01:56:55 +0900

    desgin : sidebar 구현중..

--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -1,39 +1,40 @@
-import Select from "react-select";
-
-const options = [
-  {
-    label: "Finland",
-    options: [
-      {
-        label: "Great Hotel",
-        value: "Great Hotel",
-      },
-      // More options
-    ],
-  },
-  {
-    label: "Sweden",
-    options: [{ label: "Stockholm", value: "Stockholm" }],
-  },
-];
+import { Arrow } from "../_components/Icons";
 
 export default function Sidebar() {
   return (
-    <div className="w-[30.4rem] bg-white dark:bg-[var(--main-bg-color)] text-black dark:text-white">
-      <div>
-        <Select
-          options={options}
-          isMulti
-          closeMenuOnSelect={false}
-          hideSelectedOptions={false}
-        />
+    <div className="w-[30.4rem] bg-white dark:bg-[var(--main-bg-color)] text-black dark:text-white border-[var(--hover-bg-color)] border-l border-t rounded-tl-3xl">
+      <div className="pl-4 pr-4 w-full h-[47.4px] border-b border-b-black dark:border-b-[var(--hover-bg-color)]">
+        <button className="w-full h-full leading-[47.4px] flex justify-between items-center text-2xl font-bold">
+          <span>구름 FE3 4월팀 스터디</span>
+          <Arrow width={14} height={14} color={"#000 var(--main-bg-white)"} />
+        </button>
       </div>
-      <div>
+      <div className="">
         <button>이벤트</button>
       </div>
       <div>
-        <div></div>
-        <div></div>
+        <ul>
+          <li className="flex items-center gap-2">
+            <span className="text-xl font-semibold">채팅 채널</span>
+            <button>
+              <Arrow
+                width={12}
+                height={12}
+                color={"#000 var(--main-bg-white)"}
+              />
+            </button>
+          </li>
+          <li className="flex items-center gap-2">
+            <span className="text-xl font-semibold">음성 채널</span>
+            <button>
+              <Arrow
+                width={12}
+                height={12}
+                color={"#000 var(--main-bg-white)"}
+              />
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
---
+++ src/_components/Icons.jsx
@@ -0,0 +1,21 @@
+export const Arrow = ({ width, height, color }) => {
+  return (
+    <svg
+      class="icon__29444"
+      color={color}
+      aria-hidden="true"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M5.3 9.3a1 1 0 0 1 1.4 0l5.3 5.29 5.3-5.3a1 1 0 1 1 1.4 1.42l-6 6a1 1 0 0 1-1.4 0l-6-6a1 1 0 0 1 0-1.42Z"
+        class=""
+      ></path>
+    </svg>
+  );
+};
